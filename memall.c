478b0d6170401c7ca6c4a131b6a0462c1caffa76
LeadConsult <leadconsultnig@gmail.com>
2022-09-07 14:38:57 -0700

    commit_05

---
+++ memall.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * * realloc_p - reallocates a pointer to double  space
+ * * @ptr: pointer to  old array
+ * * @size: pointer to no of elements in  old array
+ * *
+ * * Return: pointer to  new array
+ */
+char **realloc_p(char **ptr, size_t *size)
+{
+	char **new;
+	size_t index;
+
+	new = malloc(sizeof(char *) * ((*size) + 10));
+	if (new == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (index = 0; index < (*size); index++)
+	{
+		new[index] = ptr[index];
+	}
+	*size += 10;
+	free(ptr);
+	return (new);
+}
+
